df5db0ac65d43c21d473c24eb16845690dff930a
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-14 14:11:32 -0700

    add delete route

--- app.js
+++ app.js
@@ -96,6 +96,18 @@ app.put("/blogs/:id", function(req, res) {
 	});
 });
 
+// Delete Route
+app.delete("/blogs/:id", function(req, res) {
+	// destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
+
 var port = process.env.PORT || 3000
 app.listen(port, process.env.IP, function() {
 	console.log("Server is running!");
